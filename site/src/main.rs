5a4fe38b13da3c3139d6169e1089575549fcf339
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-03-30 23:28:47 -0700

    fix weewah sound link

--- site/src/main.rs
+++ site/src/main.rs
@@ -142,7 +142,7 @@ pub fn MainPage(cx: Scope) -> impl IntoView {
 #[component]
 fn MainText(cx: Scope) -> impl IntoView {
     view! { cx, <div>
-        <p>"Uiua ("<i><a href="weewah.mp3">"wee-wuh"</a></i>") is a stack-oriented array programming language with a focus on tacit code (code without named values). Its semantics and primitives (and this site) are largely inspired by "<a href="https://mlochbaum.github.io/BQN/">"BQN"</a>", but it combines the array paradigm with the stack-oriented paradigm to make writing point-free code more workable."</p>
+        <p>"Uiua ("<i><a href="https://www.uiua.org/weewah.mp3">"wee-wuh"</a></i>") is a stack-oriented array programming language with a focus on tacit code (code without named values). Its semantics and primitives (and this site) are largely inspired by "<a href="https://mlochbaum.github.io/BQN/">"BQN"</a>", but it combines the array paradigm with the stack-oriented paradigm to make writing point-free code more workable."</p>
         <hr/>
         <h3>"How is Uiua like other array languages?"</h3>
         <p>"Like APL, J, BQN, and many other array languages, Uiua's primary data type is the array. Arrays are multidimensional and rank-polymorphic, meaning that many operation automatically apply to every element."</p>

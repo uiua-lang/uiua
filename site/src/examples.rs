c44c7415f1cb46d21a10f2357f0f918eeb000c41
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-04-05 08:10:16 -0700

    update some examples

--- site/src/examples.rs
+++ site/src/examples.rs
@@ -2,17 +2,17 @@ const UIUA: &str = r#"‡=¯1 ≡/-◫2 <'A' ⊂' '."Um, I um...arrays""#;
 const FORMAT: &str = "# Click Run to format!\nfirst repeat (\\+ rev)0_1 10";
 const D3: &str = "↯~⇡/×.2_3_4";
 const LOGO: &str = "\
-x ← ⊞⚇.÷÷2 ~-÷2,⇡. 256
+x ← ⊞⊟.÷÷2 ~-÷2,⇡. 256
 rgb ← ⊞: (×.⊢⇌)_(×.⊢)_0.5 x
 u ← ∵(↥<0.2 ~>0.7.+×2 ×.~/·) x
 c ← √∵(/+ ⁿ2) x
 ⍉⊂~-¬u <1 c +0.1 ≡(↧<0.95 c) rgb";
 const AVG: &str = "Avg ← ÷⇀~/+.\nAvg 0_2_1_5";
-const QUADRATIC: &str = "{÷×2 a -b ⚇¯.√-××4 a c ⁿ2 b}1 2 0";
+const QUADRATIC: &str = "{÷×2 a -b ⊟¯.√-××4 a c ⁿ2 b}1 2 0";
 const STRIPES: &str = "\
-r ← ⌵sin ÷10 ⊞+.⇡300 
-g ← ⌵sin ÷21 ⊞↥.⇡300 
-b ← ⌵sin ÷32 ⊞-.⇡300 
+r ← ⌵sin ÷10 ⊞+.⇡300
+g ← ⌵sin ÷21 ⊞↥.⇡300
+b ← ⌵sin ÷32 ⊞-.⇡300
 ⍉[r g b]";
 const PALINDROME: &str = r#"⊟∵(≅⇌.).⊢⊕=' '."uiua racecar wow cool!""#;
 const RULE_30: &str = "\
@@ -23,8 +23,8 @@ start ← =÷2 size ⇡+1 size
 const PRIMES: &str = "‡∵(=2 /+ =⌊.÷⇡.).+1 ⇡60";
 const TRANS_STACK: &str = "⍉↯4_4[...1 .2 .3 ...4 .5 .6]";
 const MANDELBROT: &str = "\
-Z ← +⚇/- ⁿ2 ~×2 /×.⇌
-⍉⊞⚇.÷25 -÷2 ~⇡. 100
+Z ← +⊟/- ⁿ2 ~×2 /×.⇌
+⍉⊞⊟.÷25 -÷2 ~⇡. 100
 ∵(<1 √/+ ⁿ2;~⍥(Z ~,) 0_0 10)";
 const CHART: &str = "∵(⊙~·_:_÷_≡_⍋ ⁅÷23)⊞×.-10 ⇡20";
 const ERRORS: &str = "# Change this ↓ to a 0\n!\"Oh no bad!\" 1\nprintln \"All is well\"";

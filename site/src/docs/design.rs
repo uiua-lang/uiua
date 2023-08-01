0c2ff903ece019945b2ac807a275a50077b86b94
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-08-01 12:46:40 -0700

    add section about tacit code to design

--- site/src/docs/design.rs
+++ site/src/docs/design.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 use uiua::primitive::Primitive;
 
-use crate::code::PrimCode;
+use crate::{code::PrimCode, editor::Editor};
 
 #[component]
 pub fn Design(cx: Scope) -> impl IntoView {
@@ -13,11 +13,27 @@ pub fn Design(cx: Scope) -> impl IntoView {
         <br/>
         <h2>"Stack Orientation"</h2>
         <hr/>
+        <h3>"Combinators"</h3>
         <p>"When I first started developing Uiua, it was neither stack-oriented nor array-oriented. What it "<em>"did"</em>" focus a lot on was "<em>"combinators"</em>". I had this whole heirarchy of language-level operators that let you construct arbitrarily complex combinators relatively succinctly."</p>
         <p>"I discovered what a lot of others have discovered when delving deep into tacit code: it's really hard to read and write and reason about."</p>
         <p>"Eventually, I moved to a stack-oriented model and discovered that you can write almost any 1 or 2 argument combinator with just "<PrimCode prim=Dup/>", "<PrimCode prim=Over/>", and "<PrimCode prim=Flip/>"."</p>
         <p>"Of course, I also made the discovery that juggling 3 or more values on the stack also imposes a high cognitive load on the developer, which is why, unlike some other stack-oriented languages, Uiua does not have a rotate function."</p>
         <p>"Thankfully, the array paradigm saved the day with the idea of dfns, which I expanded to be able to take way more arguments, so you can still write complex yet concise combinators."</p>
+        <br/>
+        <h3>"Expressions"</h3>
+        <p>"Long tacit expressions in most array languages can get very unwieldy. Because binary operations are infix, you have to parse the tree structure in your head before you can start determining the order of operations."</p>
+        <p>"For example, in BQN, you can trim matches from the beginning of a string with "<a style="text-decoration: none;" href="https://mlochbaum.github.io/bqncrate/?q=Remove%20cells%20that%20appear%20in%20x%20from%20beginning%20of%20y#"><code>"x(∧`∘∊˜¬⊸/⊢)y"</code></a>". "</p>
+        <p>"In contrast, here is there equivalent in Uiua, implemented the same way:"</p>
+        <Editor example="Trim ← ‡¬\\×∊,\n"/>
+        <p>
+            "You'll notice that stack orientation simplifies the expression in a few ways:"
+            <ul>
+                <li>"There is no Uiua code corresponding to the BQN combinators "<code>"∘"</code>" and "<code>"⊸"</code>" and the identity function "<code>"⊢"</code>". Function composition is implicit."</li>
+                <li>"Functions are executed right-to-left instead of in a right-to-left tree ordering."</li>
+                <li>"The expression does not require "<code>"()"</code>"s. In fact, no Uiua expression requires explicit grouping. "<code>"()"</code>" is used to make inline functions instead."</li>
+            </ul>
+        </p>
+        <p>"I think this clarity makes writing long tacit expression much more workable."</p>
         <br/>
         <h2>"The Flat Array Model"</h2>
         <hr/>

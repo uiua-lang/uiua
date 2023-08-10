17869d2dbced8dd62fd569c2bd8bee965e5025cf
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-08-09 17:19:13 -0700

    small site tweaks

--- site/src/examples.rs
+++ site/src/examples.rs
@@ -5,8 +5,8 @@ const LOGO: &str = "\
 x ← ↶⍉⊞⊟. ÷÷2~ -÷2,⇡.200
 rgb ← [×.⊢⇌x ×.⊢x ↯△⊢x0.5]
 u ← ↥<0.2~>0.7.+×2 ×.~/·x
-c ← √/+ⁿ2 x
-⍉⊂~-¬u <1 c +0.1 ≡(↧<0.95 c)rgb";
+C ← <~√/+ⁿ2 x
+⍉⊂~-¬u c1 +0.1 ⫫↧rgb c0.95";
 const AVG: &str = "Avg ← ÷⧻~/+.\nAvg 0_2_1_5";
 const CHORD: &str = "\
 [0 4 7 10]
@@ -22,10 +22,10 @@ const PALINDROME: &str = r#"$ uiua racecar wow cool!
 ≍⊂ ⊏~"❌✅" ≡(≅⇌.). ⊘≠' '."#;
 const RULE_30: &str = "\
 Thirty ← ≡(↥≅0_1_1 ~=1 /+.) ◫3 ⊂~0 ⊂0
-size ← 600
+size ← 500
 start ← =÷2 size ⇡+1 size
 ⇌[⍥(Thirty.)÷2 size start]";
-const PRIMES: &str = "‡∵(=2 /+ =⌊.÷⇡.).+1 ⇡60";
+const PRIMES: &str = "‡∵(=2 /+ =⌊.÷⇡.).+1 ⇡50";
 const MANDELBROT: &str = "\
 Z ← ⊟/- ⁿ2 ~×2 /×.⇌
 ⇌↶⍉⊞⊟.×4 ÷~-÷2,⇡. 300

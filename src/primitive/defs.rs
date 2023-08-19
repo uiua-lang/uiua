dac54565d2dab6688e26e7d14831d58ab78a4a24
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-08-19 15:13:13 -0700

    document antistack functions

--- src/primitive/defs.rs
+++ src/primitive/defs.rs
@@ -122,10 +122,32 @@ primitive!(
     /// ex: () # Try running to format
     (0(0), Noop, Stack, ("noop", '·')),
     /// Move a value from the stack to the antistack
+    ///
+    /// Get the value back with [load].
+    /// ex: [↓ 1 2 3 ↑4 5]
+    ///
+    /// If you want to temporarily get a value on the top of the stack out of the way while you work on values below it, you can use [under][save].
+    /// ex: [⍜↑+ 1 2 3 4]
+    /// You can use as many `save`s as values you want to move out of the way.
+    /// ex: [⍜(↑↑↑)+ 1 2 3 4 5]
+    /// For deep stack manipulation, use this with [repeat].
+    /// ex: [⍜(⍥↑8)+ 1 2 3 4 5 6 7 8 9 10]
     (1(0), Save, Stack, ("save", AsciiToken::Caret, '↑')),
     /// Move a value from the antistack to the stack
+    ///
+    /// Move values to the antistack in the first place with [save].
+    /// ex: [↓ 1 2 3 ↑4 5]
+    ///
+    /// [load] is equivalent to (and formats from) [anti][noop].
+    /// ex: [~() 2 3 ↑4 5]
     (0(1), Load, Stack, ("load", '↓')),
-    /// Call a function on the antistack
+    /// Call a function on the antistack and move the result to the stack
+    ///
+    /// [anti][noop] is equivalent to (and formats to) [load].
+    /// ex: [~() 2 3 ↑4 5]
+    ///
+    /// Combining [anti] with [duplicate] and [over] gives you a way to randomly sample up to two values from the antistack.
+    /// ex: [↓↓~,~.~,~,~.] ↑↑ 0 1
     ([1], Anti, Stack, ("anti", '~')),
     // Pervasive monadic ops
     /// Logical not

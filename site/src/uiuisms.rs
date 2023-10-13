99a7cca319022d15e96d56a3279e3049212f7c6a
Pedro Cabral <17774851+cabralski@users.noreply.github.com>
2023-10-13 00:14:42 -0300

    added arithmetic mean to uiuisms.rs
    i hope this implementation is simple and good enough. what an incredible language!

--- site/src/uiuisms.rs
+++ site/src/uiuisms.rs
@@ -193,6 +193,8 @@ uiuisms!(
     "+⊙'×-. 0 10 0.2",
     /// Set the value of an array at an index
     "⍜⊏; 2 1_2_3_4 10",
+    /// Arithmetic mean
+    "÷ ⧻ ∶ /+ . [85 105 117 97]",
     /// Dot product
     "/+× [1 2 3] [4 ¯5 6]",
     /// Cross product

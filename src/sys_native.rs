ffa959a4850f171d9565a918a84ebfac2cce6b95
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-10-27 21:03:08 -0700

    fix site build

--- src/sys_native.rs
+++ src/sys_native.rs
@@ -127,6 +127,7 @@ impl SysBackend for NativeSys {
                 .read_exact(slice::from_mut(&mut b))
                 .map_err(|e| e.to_string())?;
             match b {
+                #[cfg(feature = "raw_mode")]
                 b'\r'
                     if crossterm::terminal::is_raw_mode_enabled().map_err(|e| e.to_string())? =>
                 {

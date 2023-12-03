1473f2c8fbd8cb46d1b6760d10278cfb8b66f36f
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-12-03 14:45:58 -0800

    fix a crash in do signature checking

--- src/check.rs
+++ src/check.rs
@@ -310,7 +310,7 @@ impl<'a> VirtualEnv<'a> {
                     let g = self.pop_func()?;
                     let f_sig = f.signature();
                     let g_sig = g.signature();
-                    let copy_count = g_sig.args.saturating_sub(g_sig.outputs - 1);
+                    let copy_count = g_sig.args.saturating_sub(g_sig.outputs.saturating_sub(1));
                     let g_sub_sig = Signature::new(g_sig.args, g_sig.outputs + copy_count - 1);
                     let comp_sig = f_sig.compose(g_sub_sig);
                     self.handle_args_outputs(

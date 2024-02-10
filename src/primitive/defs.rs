1046f451b26010c78666df1c1693d7cc8bb64e45
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-02-10 00:51:27 -0800

    fix a typo in atan docs

--- src/primitive/defs.rs
+++ src/primitive/defs.rs
@@ -462,7 +462,7 @@ primitive!(
     /// ex: ∠ ¯1 0
     /// ex: ∠ √2 √2
     ///
-    /// [un][atan] gives the [sine] and `cosine` of an angle.
+    /// [un][atangent] gives the [sine] and `cosine` of an angle.
     /// ex: °∠ 0
     /// ex: °∠ η
     /// ex: °∠ π

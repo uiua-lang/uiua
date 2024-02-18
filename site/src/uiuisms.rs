a7ed2099bcf3e719328386260c898a1eb669fc95
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-02-18 11:15:49 -0800

    small uiuism fix

--- site/src/uiuisms.rs
+++ site/src/uiuisms.rs
@@ -210,7 +210,7 @@ uiuisms!(
     /// Trim whitespace
     r#"▽×⍜(⊟⇌)≡\↥.≠@ . "  abc xyz   ""#,
     /// Upscale a 2d matrix
-    "30 [0_1 1_0]\n▽▽⧻⟜:⍉▽▽⧻,⟜:",
+    "30 [0_1 1_0.5]\n°⍉▽▽⧻⟜:⍉▽▽⧻,⟜:",
     /// Upscale a colored image
     "30 [[0_0_1 0_1_0] [1_0_0 0_0_0]]\n°⍉▽▽⧻⟜:⍉▽▽⧻,⟜:",
     /// Linearly interpolate between two values

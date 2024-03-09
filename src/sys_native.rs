964cdc931e03b8e45e8bff43855aa331b2ac4239
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-03-08 18:32:10 -0800

    Revert "fix file and tcp reading"
    This reverts commit a80f58cea3543f0644b6b133ac1ee54174ddb1dd.

--- src/sys_native.rs
+++ src/sys_native.rs
@@ -228,8 +228,10 @@ impl SysBackend for NativeSys {
     fn read(&self, handle: Handle, len: usize) -> Result<Vec<u8>, String> {
         Ok(match NATIVE_SYS.get_stream(handle)? {
             SysStream::File(mut file) => {
-                let mut buf = vec![0; len];
-                file.read_exact(buf.as_mut_slice())
+                let mut buf = Vec::new();
+                Write::by_ref(&mut *file)
+                    .take(len as u64)
+                    .read_to_end(&mut buf)
                     .map_err(|e| e.to_string())?;
                 buf
             }
@@ -242,9 +244,10 @@ impl SysBackend for NativeSys {
             }
             SysStream::TcpListener(_) => return Err("Cannot read from a tcp listener".to_string()),
             SysStream::TcpSocket(mut socket) => {
-                let mut buf = vec![0; len];
-                socket
-                    .read_exact(buf.as_mut_slice())
+                let mut buf = Vec::new();
+                Write::by_ref(&mut *socket)
+                    .take(len as u64)
+                    .read_to_end(&mut buf)
                     .map_err(|e| e.to_string())?;
                 buf
             }

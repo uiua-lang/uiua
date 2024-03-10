5655842d1096c217d3dbcc7506d176d08eb7715b
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-03-09 21:25:22 -0800

    fix some of by's docs

--- src/primitive/defs.rs
+++ src/primitive/defs.rs
@@ -1548,12 +1548,12 @@ primitive!(
     ([1], On, Stack, ("on", '⟜')),
     /// Duplicate a function's last argument before calling it
     ///
-    /// If you wanted to filter out every element of an array that is not [less than] 10, you could use [keep].
+    /// If you want to filter out every element of an array that is not [less than] 10, you can use [keep].
     /// ex: ▽<10. [1 27 8 3 14 9]
-    /// However, if you wanted to make this a function, you would have to [dip] below the bound to [duplicate] the array.
+    /// However, if you want to make this a function, you have to [dip] below the first arguement to [duplicate] the array.
     /// ex: F ← ▽<⊙.
     ///   : F 10 [1 27 8 3 14 9]
-    /// While this works, it may be take a moment to process in your mind how the stack is changing.
+    /// While this works, it may take a moment to process in your mind how the stack is changing.
     /// [by] expresses the common pattern of performing an operation but preserving the last argument so that it can be used again.
     /// With [by], the filtering function above can be written more simply.
     /// ex: # Experimental!

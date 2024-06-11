0a585b573a9d8ff8822808b1b4fc67b5284c7cfa
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-06-11 14:50:40 -0700

    give reasoning for removal on uiuisms page

--- site/src/uiuisms.rs
+++ site/src/uiuisms.rs
@@ -15,5 +15,10 @@ pub fn Uiuisms() -> impl IntoView {
         <Title text="Uiuisms - Uiua Docs"/>
         <h1>"Uiuisms"</h1>
         <p>"This page has been removed."</p>
+        <ul>
+            <li>"It provided a 'definitive' implementation of many algorithms where the best implementation is subjective and/or context-dependent"</li>
+            <li>"It is not how I want people to approach learning Uiua"</li>
+            <li>"It is not something I am interested in maintaining"</li>
+        </ul>
     }
 }

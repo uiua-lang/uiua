d8b47aec201296eadbc50ca5402c7409702df784
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-10-01 20:08:00 -0400

    filled csv

--- src/algorithm/monadic.rs
+++ src/algorithm/monadic.rs
@@ -1885,10 +1885,15 @@ impl Value {
         return Err(env.error("CSV support is not enabled in this environment"));
         #[cfg(feature = "csv")]
         {
+            let delimiter = u8::try_from(env.char_scalar_fill().unwrap_or(','))
+                .map_err(|_| env.error("CSV delimiter must be ASCII"))?;
+
             let mut buf = Vec::new();
             let mut writer = csv::WriterBuilder::new()
                 .flexible(true)
+                .delimiter(delimiter)
                 .from_writer(&mut buf);
+
             match self.rank() {
                 0 => writer
                     .write_record([self.format()])
@@ -1983,17 +1988,21 @@ impl Value {
                 .map_err(|e| env.error(e))
         }
     }
-    pub(crate) fn from_csv(_csv: &str, env: &mut Uiua) -> UiuaResult<Self> {
+    pub(crate) fn from_csv(csv_str: &str, env: &mut Uiua) -> UiuaResult<Self> {
         #[cfg(not(feature = "csv"))]
         return Err(env.error("CSV support is not enabled in this environment"));
         #[cfg(feature = "csv")]
         {
+            let delimiter = u8::try_from(env.char_scalar_fill().unwrap_or(','))
+                .map_err(|_| env.error("CSV delimiter must be ASCII"))?;
+
             let mut reader = csv::ReaderBuilder::new()
                 .has_headers(false)
                 .flexible(true)
-                .from_reader(_csv.as_bytes());
-            let fill = env.value_fill().cloned().unwrap_or_else(|| "".into());
-            env.with_fill(fill, |env| {
+                .delimiter(delimiter)
+                .from_reader(csv_str.as_bytes());
+
+            env.with_fill("".into(), |env| {
                 let mut rows = Vec::new();
                 for result in reader.records() {
                     let record = result.map_err(|e| env.error(e))?;

3e1c629703070914ff961ae544807cc5f856a2c1
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-10-03 21:04:09 -0700

    fix some doc formatting

--- src/sys.rs
+++ src/sys.rs
@@ -369,9 +369,7 @@ sys_op! {
     ///
     /// How the image is shown depends on the system backend.
     ///
-    /// In the default backend, the image is shown in the terminal. Here you can make it use sixel
-    /// by setting the `UIUA_ENABLE_SIXEL` environment variable to `1`. Otherwise it will try to use the
-    /// `kitty` or `iTerm` image protocols and fall back to half-block image printing.
+    /// In the default backend, the image is shown in the terminal. Here you can make it use sixel by setting the `UIUA_ENABLE_SIXEL` environment variable to `1`. Otherwise it will try to use the `kitty` or `iTerm` image protocols and fall back to half-block image printing.
     /// On the web, the image is shown in the output area.
     ///
     /// The image must be a rank 2 or 3 numeric array.

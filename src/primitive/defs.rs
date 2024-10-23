6a9d850664fd370383c4f66944508a9dcbab07e7
JoaoFelipe3 <ccristiany339@gmail.com>
2024-10-23 17:51:17 -0300

    Make 'base' docs use '⌝ anti'

--- src/primitive/defs.rs
+++ src/primitive/defs.rs
@@ -1714,15 +1714,15 @@ primitive!(
     ///   : ⍜(°⍉base4|⬚0↙3) [10 100 1000]
     /// It can also be used with [un][on] to convert digits in a certain base back to numbers.
     /// ex: # Experimental!
-    ///   : ◌°⟜base 2 [1 0 0 1 0]
-    ///   : ◌°⟜base 2 [1_0_0 0_1_1 1_1_1]
-    ///   : ◌°⟜base 10 [1 2 3]
+    ///   : ⌝base 2 [1 0 0 1 0]
+    ///   : ⌝base 2 [1_0_0 0_1_1 1_1_1]
+    ///   : ⌝base 10 [1 2 3]
     /// For a scalar base, this is equivalent to evaluating a polynomial.
     /// The polynomial x²-2x+1 could be represented like this:
     /// ex: # Experimental!
-    ///   : ◌°⟜base 0 [1 ¯2 1]
-    ///   : ◌°⟜base 1 [1 ¯2 1]
-    ///   : ◌°⟜base 2 [1 ¯2 1]
+    ///   : ⌝base 0 [1 ¯2 1]
+    ///   : ⌝base 1 [1 ¯2 1]
+    ///   : ⌝base 2 [1 ¯2 1]
     (2, Base, DyadicArray, "base"),
     /// Get all combinations of `k` rows from an array
     ///

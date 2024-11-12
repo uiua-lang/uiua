a4eabf06fc5c602427cbc57ddf7fad8505a97d6a
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-11-12 09:49:15 -0800

    validate repeat with large stack repetition count

--- src/check.rs
+++ src/check.rs
@@ -13,7 +13,10 @@ use std::{
 
 use serde::*;
 
-use crate::{Array, ArrayLen, ImplPrimitive, Node, Primitive, SigNode, Signature, Value};
+use crate::{
+    algorithm::validate_size_of, Array, ArrayLen, ImplPrimitive, Node, Primitive, SigNode,
+    Signature, Value,
+};
 
 impl Node {
     /// Get the signature of this node
@@ -97,9 +100,17 @@ impl Stack {
         self.set_min_height();
         self.stack.pop().unwrap_or(BasicValue::Other)
     }
+    fn pop_n(&mut self, n: usize) {
+        self.height = self.height.saturating_sub(n as i32);
+        self.stack.truncate(self.stack.len().saturating_sub(n));
+    }
     fn push(&mut self, val: BasicValue) {
         self.height += 1;
         self.stack.push(val);
+    }
+    pub fn push_n(&mut self, n: usize) {
+        self.height = self.height.saturating_add(n as i32);
+        self.stack.extend(repeat(BasicValue::Other).take(n));
     }
     /// Set the current stack height as a potential minimum.
     /// At the end of checking, the minimum stack height is a component in calculating the signature.
@@ -580,12 +591,8 @@ impl VirtualEnv {
         self.stack.pop()
     }
     fn handle_args_outputs(&mut self, args: usize, outputs: usize) {
-        for _ in 0..args {
-            self.pop();
-        }
-        for _ in 0..outputs {
-            self.push(BasicValue::Other);
-        }
+        self.stack.pop_n(args);
+        self.stack.push_n(outputs);
     }
     fn handle_sig(&mut self, sig: Signature) {
         self.handle_args_outputs(sig.args, sig.outputs)
@@ -604,6 +611,9 @@ impl VirtualEnv {
                             ((n - 1) * (sig.args - sig.outputs) + sig.args, sig.outputs)
                         }
                     };
+                    if validate_size_of::<BasicValue>([outputs]).is_err() {
+                        return Err("repeat with excessive outputs".into());
+                    }
                     self.handle_args_outputs(args, outputs);
                 }
             } else if n.is_infinite() {

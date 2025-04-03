93c4ceffce91a2559bf93c4f64d5c30372eed2f2
Kai Schmidt <kaikaliischmidt@gmail.com>
2025-04-03 11:01:51 -0700

    improve some examples

--- site/src/examples.rs
+++ site/src/examples.rs
@@ -24,18 +24,18 @@ keepnotbymember  # Keep not in list";
 pub const LOGO: &str = "\
 U ← /=⊞<0.2_0.7 /+×⟜ⁿ1_2
 I ← >⌵/ℂ: # Circle
-u ← +0.1↧¤ ⊃(I0.95|⊂:0.5⇌°√)
+u ← +0.1↧¤ ⊃(I0.95|⊂⊙0.5⇌°√)
 A ← ×⊃U(I1) # Alpha
 ⍜°⍉(⊂⊃u A) -1÷÷2⟜(⇡↯2) 200";
 pub const WEEWUH_LOGO: &str = "\
 Wee ← /=⊞<¯0.5_¯0.1 /+⍜⊣(+⊃(×4ⁿ4|×¯3ⁿ2))
 w   ← >⌵/ℂ: # Circle
-u   ← +0.1↧¤ ⊃(w0.95|⊂:0.5⇌°√)
+u   ← +0.1↧¤ ⊃(w0.95|⊂⊙0.5⇌°√)
 h   ← ×⊃Wee(w1) # Alpha
 ⍜°⍉(⊂⊃u h) -1÷÷2⟜(⇡↯2) 200";
 const CHORD: &str = "\
 [0 4 7 10]     # Notes
-×220 ⁿ:2÷12    # Freqs
+×220 ˜ⁿ2÷12    # Freqs
 ∿×τ ⊞× ÷⟜⇡&asr # Generate
 ÷⧻⟜/+⍉         # Mix";
 const SPIRAL: &str = "\
@@ -43,23 +43,23 @@ const SPIRAL: &str = "\
 -≡⊃∠(⍜∩°√+)⊸⍉     # Spiral field
 -π◿τ⊞-×τ÷⟜⇡20     # Animate";
 const QUADRATIC: &str = "\
-Disc ← -⊃(××4⊙⋅∘)⋅(×.)
-Quad ← ÷⊃(×2|-⊃⋅∘(⊟¯.√ℂ0 Disc))
+Disc ← -⊃(××4⊙⋅∘)⋅°√
+Quad ← ÷⊃(×2|-⊃⋅∘(⊟⊸¯ √ℂ0 Disc))
 Quad 1 ¯3 2";
 const STRIPES: &str = "\
 [⊞⊃⊃+↥-].⇡300
 ⍉ ÷2 +1.2 ∿ ÷10";
 pub const PALINDROME: &str = r#"$ uiua racecar wow cool!
-⬚@ ⊜(⊂⊏:"❌✅" ≍⊸⇌.)⊸≠@ "#;
+≡⊂ ˜⊏"❌✅" ⬚@ ⊜⊸(≍⊸⇌)⊸≠@ "#;
 pub const WEEWUH_PALINDROME: &str = r#"$ weewuh racecar wow cool!
-⬚@ ⊜(⊂⊏:"❌✅" ≍⊸⇌.)⊸≠@ "#;
+≡⊂ ˜⊏"❌✅" ⬚@ ⊜⊸(≍⊸⇌)⊸≠@ "#;
 const AUTOMATA: &str = "\
-Rule ← /+⊞= ⊓(⊚⋯|°⋯⇌⧈∘3⇌ ⊂⊂0:0)
+Rule ← /+⊞= ⊓(⊚⋯|°⋯⇌⧈∘3⇌ ⊂⊂0⊙0)
 =⌊÷2⟜⇡ 500          # Init
 ⇌[◌⍥⟜⊸Rule⌊÷2⧻⤙] 30 # Run";
-const ROMAN: &str = r#"k ← "IVXLCDM"
-n ← [1 5 10 50 100 500 1000]
-F ← /+-⊃(↻1×|×¬)⊸(⧈>⊂:0)⊏:n⊗:k
+const ROMAN: &str = r#"K ← "IVXLCDM"
+N ← [1 5 10 50 100 500 1000]
+F ← /+-⊃(↻1×|×¬)⊸(⧈>⊂⊙0) ⊏⊙N ⊗⊙K
 F "LVII"
 F "MCMXCIV""#;
 const MANDELBROT: &str = "\

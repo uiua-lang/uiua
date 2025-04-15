e74073458b6e0900650caf91fc38bdbcf01bd063
Kai Schmidt <kaikaliischmidt@gmail.com>
2025-04-15 14:35:43 -0700

    add note about kakoune editor support to installation page

--- site/src/other.rs
+++ site/src/other.rs
@@ -84,6 +84,7 @@ pub fn Install() -> impl IntoView {
         <p>"For Vim, sputnick1124 maintains a "<a href="https://github.com/sputnick1124/uiua.vim">{lang}" plugin"</a>"."</p>
         <p>"Language support in Neovim is easy with "<a href="https://github.com/neovim/nvim-lspconfig">"nvim-lspconfig"</a>"."</p>
         <p>"For Emacs, crmsnbleyd maintains a "<a href="https://github.com/crmsnbleyd/uiua-ts-mode">{lang}" mode"</a>"."</p>
+        <p>"For Kakoune, ThaCuber maintains a "<a href="https://github.com/thacuber2a03/highlighters.kak/blob/main/uiua.kak">"syntax highlighting module"</a>"."</p>
         <p>"These require "{lang}" to be installed and in your "<code>"PATH"</code>"."</p>
 
         <Hd id="basic-usage">"Basic Usage"</Hd>

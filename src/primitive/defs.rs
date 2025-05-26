1236d073397b8fb15ddb72a7d4c6329708c797ae
regr4 <60316183+regr4@users.noreply.github.com>
2025-05-26 11:42:57 +0200

    Add note about optimization to `path` documentation

--- src/primitive/defs.rs
+++ src/primitive/defs.rs
@@ -3023,7 +3023,7 @@ primitive!(
     ///   : ⊓$Path$Cost
     /// As stated before, the costs can be omitted. Notice [un][box]ing is not necessary in this case, and a cost is not returned.
     /// ex: ⊢ path(+A₂¤)≍ 0_0 3_5
-    /// In the examples above, we use [first] to get only the first path. [first][path] and [pop][path] are optimized to not do extra work.
+    /// In the examples above, we use [first] to get only the first path. [first][path], [pop][path] and [sign][length][path] are optimized to not do extra work.
     /// If we want *all* shortest paths, we can omit [first].
     /// ex: path(+A₂¤)≍ 0_0 1_2
     /// If pathing on a grid like the examples above, we can use [un][where] to visualize the path that was taken!

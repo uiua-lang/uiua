a20d870a7a8ca18df30c8bb5fd3bff122c6483b7
Krftdnr <32688710+Krftdnr@users.noreply.github.com>
2025-07-09 22:35:23 -0400

    Remove reciprocal power optimization (#770)

--- src/compile/optimize.rs
+++ src/compile/optimize.rs
@@ -122,7 +122,6 @@ static OPTIMIZATIONS: &[&dyn Optimization] = &[
     &((SortDown, Reverse), Sort),
     &((Pop, Rand), ReplaceRand),
     &((Pop, Pop, Rand), ReplaceRand2),
-    &((1, Flip, Div, Pow), Root),
     &((-1, Pow), (1, Flip, Div)),
     &((2, Pow), (Dup, Mul)),
     &((Complex, Abs), AbsComplex),

71340732f6f832ae9fac80043739a8422c4e05e0
Tau <git@alice-carroll.pet>
2025-07-18 02:01:08 +0300

    ci: run clippy on all crates

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1232,6 +1232,7 @@ pub(crate) fn run_sys_op(op: &SysOp, env: &mut Uiua) -> UiuaResult {
                 .change_directory(&path)
                 .map_err(|e| env.error(e))?;
         }
+        #[cfg_attr(not(feature = "image"), expect(clippy::let_unit_value))]
         SysOp::WebcamCapture => {
             let index = env.pop(1)?.as_nat(env, "Webcam index must be an integer")?;
             let _image = (env.rt.backend)

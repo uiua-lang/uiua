077ef39ea144477fa1b31ad756a5d8c354c914ad
Kai Schmidt <kaikaliischmidt@gmail.com>
2025-08-03 12:33:47 -0700

    fix a join error message

--- src/algorithm/dyadic/combine.rs
+++ src/algorithm/dyadic/combine.rs
@@ -508,15 +508,14 @@ impl<T: ArrayValue> Array<T> {
                     if self.rank() <= other.rank() || self.rank() - other.rank() > 1 {
                         return Err(C::fill_error(ctx.error(format!(
                             "Cannot join rank {} array with rank {} array{e}",
-                            other.rank(),
-                            self.rank()
+                            self.rank(),
+                            other.rank()
                         ))));
                     }
                     if self.shape[1..] != other.shape {
                         return Err(C::fill_error(ctx.error(format!(
                             "Cannot join arrays of shapes {} and {}{e}",
-                            other.shape,
-                            FormatShape(&self.shape[1..]),
+                            self.shape, other.shape
                         ))));
                     }
                 }

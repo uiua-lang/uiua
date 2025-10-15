845bfaeac6b48a74f52faa10e87a07d1414bbf84
ElNico56 <meeqnico@gmail.com>
2025-10-15 04:05:07 -0300

    rescale domain coloring hsv

--- src/algorithm/media.rs
+++ src/algorithm/media.rs
@@ -1,6 +1,6 @@
 //! En/decode Uiua arrays to/from media formats
 
-use std::f64::consts::E;
+use std::f64::consts::TAU;
 
 use ecow::{eco_vec, EcoVec};
 use enum_iterator::{all, Sequence};
@@ -434,9 +434,9 @@ pub fn value_to_image(value: &Value) -> Result<DynamicImage, String> {
 fn complex_color(c: Complex) -> [f64; 3] {
     let h = c.arg();
     let mag = c.abs();
-    let s = 0.3 + 0.7 * (-mag / 10.0).exp();
-    let v = 1.0 - (-E * mag).exp();
-    hsv_to_rgb(h, s, v)
+    let s = (0.3 + 0.7 * (-mag / 10.0).exp()) / (0.3 + 0.7 * (-0.1 as f64).exp());
+    let v = (1.0 - (-TAU * mag).exp()) / (1.0 - (-TAU).exp());
+    hsv_to_rgb(h, s.min(1.0), v.min(1.0))
 }
 
 #[doc(hidden)]

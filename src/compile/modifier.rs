4233c00a13574ea90213818effcc6185cb5a7a3d
Kai Schmidt <kaikaliischmidt@gmail.com>
2026-01-15 17:54:03 -0800

    working on fold while

--- src/compile/modifier.rs
+++ src/compile/modifier.rs
@@ -302,14 +302,14 @@ impl Compiler {
                 let span = self.add_span(modifier.span.clone());
                 Ok(Node::ImplMod(ImplPrimitive::Astar, args, span))
             }
-            // Modifier::Primitive(Primitive::Fold) if pack.branches.len() == 2 => {
-            //     let mut args = pack.lexical_order().cloned().map(|w| w.map(Word::Func));
-            //     let f = self.word_sig(args.next().unwrap())?;
-            //     let g = self.word_sig(args.next().unwrap())?;
-            //     let args = eco_vec![f, g];
-            //     let span = self.add_span(modifier.span.clone());
-            //     Ok(Node::ImplMod(ImplPrimitive::FoldWhile, args, span))
-            // }
+            Modifier::Primitive(Primitive::Fold) if pack.branches.len() == 2 => {
+                let mut args = pack.lexical_order().cloned().map(|w| w.map(Word::Func));
+                let f = self.word_sig(args.next().unwrap())?;
+                let g = self.word_sig(args.next().unwrap())?;
+                let args = eco_vec![f, g];
+                let span = self.add_span(modifier.span.clone());
+                Ok(Node::ImplMod(ImplPrimitive::FoldWhile, args, span))
+            }
             Modifier::Primitive(Primitive::Geometric) if pack.branches.len() == 2 => {
                 let mut args = pack.lexical_order().cloned().map(|w| w.map(Word::Func));
                 let metrics = args.next().unwrap();

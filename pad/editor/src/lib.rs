afeeaec7aeeb80a28f1044a8cb15e6405550f938
Lynn <lynn@foldr.moe>
2026-01-27 12:07:49 +0100

    Arrow icons for code buttons

--- pad/editor/src/lib.rs
+++ pad/editor/src/lib.rs
@@ -2313,18 +2313,46 @@ pub fn Editor<'a>(
                         <button
                             id="prev-example"
                             class="code-button"
+                            aria-label="Previous"
                             style=example_arrow_style
                             on:click=prev_example
                         >
-                            {"<"}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            >
+                                <path d="M19 12H5"/>
+                                <path d="M12 19l-7-7 7-7"/>
+                            </svg>
                         </button>
                         <button
                             id="next-example"
                             class=next_button_class
+                            aria-label="Next"
                             style=example_arrow_style
                             on:click=next_example
                         >
-                            {">"}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            >
+                                <path d="M5 12h14"/>
+                                <path d="M12 5l7 7-7 7"/>
+                            </svg>
                         </button>
                     </div>
                 </div>

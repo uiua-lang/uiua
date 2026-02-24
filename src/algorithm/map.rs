0cef88297f133771062f9609b6124086a6cecae9
Andrei E. <andreien@proton.me>
2026-02-24 11:36:01 +0000

    cargo fmt

--- src/algorithm/map.rs
+++ src/algorithm/map.rs
@@ -12,7 +12,7 @@ use serde::*;
 
 use crate::{
     Array, ArrayValue, Boxed, Complex, FormatShape, OptionalArg, Uiua, UiuaResult, Value,
-    algorithm::ArrayCmpSlice, val_as_arr, media::builtin_params
+    algorithm::ArrayCmpSlice, media::builtin_params, val_as_arr,
 };
 
 use super::{ErrorContext, FillContext};

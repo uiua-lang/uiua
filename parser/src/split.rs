545c9ee342b8969ddf861e06e3ddce0474ed92c8
cqn-brwpna9 <im.a.nony.moose.o@gmail.com>
2026-07-14 18:16:40 -0700

    formatted

--- parser/src/split.rs
+++ parser/src/split.rs
@@ -366,7 +366,7 @@ impl Primitive {
             ),
             alias!((p, Add), (g, PrimComponent::E), (a, PrimComponent::Sub0)),
             alias!((k, Keep), (b, By), (m, MemberOf)),
-	    alias!((k, Keep), (n, Not), (b, By), (m, MemberOf)),
+            alias!((k, Keep), (n, Not), (b, By), (m, MemberOf)),
         ]
     }
     /// Look up a multi-alias from [`Self::multi_aliases`]
